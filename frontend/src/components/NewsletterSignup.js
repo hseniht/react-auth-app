20cf19cbb5facdcb897cd1304abb89e85ac524f7
hseniht <thinesh_kumar_mu@hotmail.com>
2023-03-25 15:11:43 +0800

    "useFetcher" for NewsLetter
    This hook is used to trigger 'loader' or 'action' without actually
    loading the page, the route to which this this action/loader belongs

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,33 @@
+import { useEffect } from "react";
+import { useFetcher } from "react-router-dom";
 import classes from "./NewsletterSignup.module.css";
 
 function NewsletterSignup() {
-  return (
-    <form method="post" className={classes.newsletter}>
+  //fetcher is used whenever we want trigger action (or loader) without navigating to its page
+  // good for behind the scene request
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      // window.alert('Signup successful!')
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
+  return(
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
