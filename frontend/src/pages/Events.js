b258191e4c0731fda43c90d5abf8961b024543ed
hseniht <thinesh_kumar_mu@hotmail.com>
2023-03-25 15:40:33 +0800

    'Defer' for showing partial content
    'Defer()' allows some content to be shown while loading
    Used react <Suspense/> to load partial 'loading .. ' component while
    waiting data.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,22 +1,33 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
+  // const data = useLoaderData();
+  const { events } = useLoaderData();
 
   //ALT WAY: for error
   // if (data.isError) {
   //   return <p>{data.message}</p>;
   // }
-  const events = data.events;
-  return <EventsList events={events} />;
+  // const events = data.events;
+  // return <EventsList events={events} />;
+
+  {
+    /* <Await> will return function once promise resolve */
+  }
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading ...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-// Note: loader functions is not a react component, so no hooks, and states
-// However,this executes in the browser, so we can use browser API's
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -27,6 +38,16 @@ export async function loader() {
     //ALT WAY: for error
     // return { isError: true, message: "Could not fetch events" };
   } else {
-    return response;
+    const resData = await response.json(); //must await and parse if using 'defer()'
+    // return response;
+    return resData.events;
   }
+}
+
+// Note: loader functions is not a react component, so no hooks, and states
+// However,this executes in the browser, so we can use browser API's
+export function loader() {
+  return defer({
+    events: loadEvents(), //returns value
+  });
 }
