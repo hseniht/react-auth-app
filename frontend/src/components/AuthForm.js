6ef5eb57820cc3316022408c04dcd4139ccc43db
hseniht <thinesh_kumar_mu@hotmail.com>
2023-04-29 22:47:10 +0800

    Validate user input and output validation error

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,4 +1,4 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useActionData, useSearchParams } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
@@ -9,6 +9,8 @@ function AuthForm() {
   //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
   // }
 
+  const data = useActionData();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
 
@@ -16,6 +18,14 @@ function AuthForm() {
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
